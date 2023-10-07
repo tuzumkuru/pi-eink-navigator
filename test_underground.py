1a30d9f27a753482700fcbeaaa4958418fd34a66
tuzumkuru <tolgauzun@gmail.com>
2023-10-06 20:09:34 -0400

    Printing time differences

--- test_underground.py
+++ test_underground.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 from underground import SubwayFeed
 
 API_KEY = os.getenv('MTA_API_KEY')
@@ -11,12 +11,27 @@ feed = SubwayFeed.get(ROUTE)
 
 your_dict = feed.extract_stop_dict()
 
-
-datetime_list = your_dict['Q']['D27N']
-
+datetime_list = your_dict['Q']['D27S']
 
 # Loop through the datetime values in the list
 for dt in datetime_list:
     formatted_time = dt.strftime('%H:%M')  # Format as HH:MM
     print(formatted_time)
 
+# Get the current time in the same timezone as datetime_list
+current_time = datetime.now(datetime_list[0].tzinfo)  # Use the timezone from the first datetime object
+
+# Initialize a list to hold remaining minutes
+remaining_minutes_list = []
+
+# Loop through the datetime values in the list
+for dt in datetime_list:
+    # Calculate the time difference
+    time_difference = dt - current_time
+    # Extract remaining minutes as an integer
+    remaining_minutes = int(time_difference.total_seconds() / 60)
+    remaining_minutes_list.append(remaining_minutes)
+
+# Print the list of remaining minutes
+for i, minutes in enumerate(remaining_minutes_list):
+    print(f"Train {i + 1}: {minutes} minutes remaining")
