e6497423a47742554499771a4ef8af2456ae6a3c
tuzumkuru <tolgauzun@gmail.com>
2023-10-08 09:25:21 -0400

    Refactor variable names and screen initialization Renamed button1_pin and button2_pin to button_prev_pin and button_next_pin for better clarity. Refactored the screen initialization process to use append method for adding screens to the list. Added an additional screen (Screen2) to the screens list.

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from screen1 import Screen1
 from screen2 import Screen2
 from controller import ScreenController
 from adafruit_epd.ssd1680 import Adafruit_SSD1680
-from screen_photo import PhotoScreen
+from photo_screen import PhotoScreen
 
 if __name__ == "__main__":
-    button1_pin = 6 
-    button2_pin = 5 
+    button_prev_pin = 6 
+    button_next_pin = 5 
 
     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
     ecs = digitalio.DigitalInOut(board.CE0)
@@ -20,17 +20,17 @@ if __name__ == "__main__":
     busy = digitalio.DigitalInOut(board.D17)
 
     display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=None, rst_pin=rst, busy_pin=busy)
-
     # Set the display rotation to 270 degrees
     display.rotation = 3
 
-    screen1 = PhotoScreen("./1.png")
-    screen2 = PhotoScreen("./2.png")
-    screen3 = PhotoScreen("./3.jpg")
-    screen4 = Screen1()
-    screens = [screen1, screen2, screen3, screen4]
+    screens = []
+    screens.append(PhotoScreen("./1.png"))
+    screens.append(PhotoScreen("./2.png"))
+    screens.append(PhotoScreen("./3.jpg"))
+    screens.append(Screen1())
+    screens.append(Screen2())
 
-    controller = ScreenController(screens, display, button1_pin, button2_pin)
+    controller = ScreenController(screens, display, button_prev_pin, button_next_pin)
 
     try:
         while True:
