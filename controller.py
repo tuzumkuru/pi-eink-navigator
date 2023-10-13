bc8ea6626d2db39ebda450e1bb08b818f92ce336
tuzumkuru <tolgauzun@gmail.com>
2023-10-13 15:44:29 -0400

    Refactor ScreenController and MTAScreen classes
    Refactored the ScreenController and MTAScreen classes to improve code readability and maintainability.
    
    In the ScreenController class, renamed several methods and variables to better reflect their purpose and to follow Python's naming conventions for private methods and variables. Also, added comments to clarify the purpose of certain blocks of code.
    
    In the MTAScreen class, reorganized the code to improve readability and added comments to clarify the purpose of certain blocks of code. Also, optimized the update_list method to reduce unnecessary API calls by checking if the departure times have changed before updating the list.
    
    In the utils.py file, added a new function print_time_elapsed to measure the execution time of a function. This will be useful for performance optimization in the future.

--- controller.py
+++ controller.py
@@ -11,43 +11,42 @@ class ScreenController:
         self.button_prev_pin = button_prev_pin
         self.button_next_pin = button_next_pin
         self.button_queue = queue.Queue()
-        self.transition_in_progress = False  # Flag to indicate a screen transition in progress
+        self.screen_transition_in_progress = False  # Flag to indicate a screen transition in progress
         self.image_refresh_requested = False    #A flag to inform the process that image has been changed
 
         # Register the controller as the screen change callback
         for screen in self.screens:
-            screen.on_change(self.screen_changed_callback)
+            screen.on_change(self._screen_changed_callback)
 
         # Add event listeners for buttons
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.button_prev_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.button_next_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.button_prev_pin, GPIO.FALLING, callback=self.button_prev_callback, bouncetime=200)
-        GPIO.add_event_detect(self.button_next_pin, GPIO.FALLING, callback=self.button_next_callback, bouncetime=200)
+        GPIO.add_event_detect(self.button_prev_pin, GPIO.FALLING, callback=self._button_prev_callback, bouncetime=200)
+        GPIO.add_event_detect(self.button_next_pin, GPIO.FALLING, callback=self._button_next_callback, bouncetime=200)
 
         # Activate the initial screen
-        self.activate_screen(self.current_screen_index)
+        self._activate_screen(self.current_screen_index)
 
         # Create a thread to process the button queue continuously
         self.is_running = True
-        self.thread = threading.Thread(target=self.loop, daemon=True)
+        self.thread = threading.Thread(target=self._loop, daemon=True)
         self.thread.start()
 
     def __del__(self):
-        self.is_running = True
+        self.is_running = False
         if(self.thread):
             self.thread.join()
 
-
-    def button_prev_callback(self, channel):
-        if not self.transition_in_progress:
+    def _button_prev_callback(self, channel):
+        if not self.screen_transition_in_progress: # do not accept button presses while a previous process is running to improve user experience
             self.button_queue.put(self.button_prev_pin)
 
-    def button_next_callback(self, channel):
-        if not self.transition_in_progress:
+    def _button_next_callback(self, channel):
+        if not self.screen_transition_in_progress: # do not accept button presses while a previous process is running to improve user experience
             self.button_queue.put(self.button_next_pin)
 
-    def loop(self):
+    def _loop(self):
         while self.is_running:
             if not self.button_queue.empty():
                 channel = self.button_queue.get()
@@ -57,44 +56,42 @@ class ScreenController:
                     self.next_screen(channel)
             elif self.image_refresh_requested:
                 self.image_refresh_requested = False
-                image = self.screens[self.current_screen_index].get_image()
-                self.display_image(image)
+                self._update_image()
             else:
                 time.sleep(0.1)
 
     def prev_screen(self, channel):
-        if not self.transition_in_progress:
-            # self.transition_in_progress = True
+        if not self.screen_transition_in_progress:
+            self.screen_transition_in_progress = True
             new_index = (self.current_screen_index - 1) % len(self.screens)
-            self.deactivate_screen(self.current_screen_index)
-            self.activate_screen(new_index)
-            # self.transition_in_progress = False
+            self._deactivate_screen(self.current_screen_index)
+            self._activate_screen(new_index)
+            self.screen_transition_in_progress = False
 
     def next_screen(self, channel):
-        if not self.transition_in_progress:
-            # self.transition_in_progress = True
+        if not self.screen_transition_in_progress:
+            self.screen_transition_in_progress = True
             new_index = (self.current_screen_index + 1) % len(self.screens)
-            self.deactivate_screen(self.current_screen_index)
-            self.activate_screen(new_index)
-            # self.transition_in_progress = False
+            self._deactivate_screen(self.current_screen_index)
+            self._activate_screen(new_index)
+            self.screen_transition_in_progress = False
 
-    def activate_screen(self, screen_index):
+    def _activate_screen(self, screen_index):
         self.screens[screen_index].activate()
         self.current_screen_index = screen_index
-        image = self.screens[screen_index].get_image()
-        self.display_image(image)
+        self._update_image()
 
-    def deactivate_screen(self, screen_index):
+    def _deactivate_screen(self, screen_index):
         self.screens[screen_index].deactivate()
 
-    def screen_changed_callback(self):
+    def _screen_changed_callback(self):
         # This function will be called when a screen changes
         self.image_refresh_requested = True
-        
 
-    def display_image(self, image):
-        self.transition_in_progress = True
+    def _update_image(self):
+        image = self.screens[self.current_screen_index].get_image()
+        self.display_image(image)
+
+    def display_image(self, image):        
         self.display.image(image)
         self.display.display()
-        self.transition_in_progress = False
-        
